5a31c87782b8fdbf6069d277606de4d149c5a663
navernoe <icbd17@gmail.com>
2020-05-11 14:30:58 +0300

    webpack configured for comfortable debug (source-map specified)

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist/'),
